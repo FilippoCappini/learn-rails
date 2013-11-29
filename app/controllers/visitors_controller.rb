d064440c31f8d5a17ee544ce305de68050d57d56
Filippo Cappini <f.cappini@gmail.com>
2013-11-29 18:38:04 +0100

    Agginti test debug per il log

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,10 @@
 class VisitorsController < ApplicationController
   
   def new
+    Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'will appear in bold blue'
+    Rails.logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
+    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
   end
   
 end
